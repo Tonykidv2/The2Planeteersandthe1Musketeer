1fbd200fd2408ad82f9e8783b3e2c7b5757e390f
Jessica John <jjjohn@fullsail.edu>
2016-11-17 16:13:45 -0500

    Made few changes in third view port due to conflicts incurred during pull.

--- Graphic2/Graphic2/main.cpp
+++ Graphic2/Graphic2/main.cpp
@@ -2439,29 +2439,9 @@ void DEMO_APP::thirdViewPort()
 	float timer = (float)TimeWizard.TotalTime();
 
 	if (g_ScreenChanged)
-	POINT CUR;
-	GetCursorPos(&CUR);
-	ScreenToClient(window, &CUR);
-	//RECT* dest;  //Rectangle(1, 2, 336, 127);
-	ID3D11Texture2D* x;
-	m_text->GetResource((ID3D11Resource**)&x);
-	D3D11_TEXTURE2D_DESC desc;
-	x->GetDesc(&desc);
-	
-	SimpleMath::Rectangle dest = SimpleMath::Rectangle(100, 100, desc.Width/2, desc.Height/2);
-	
-	spritebatch->Begin();
-	spritebatch->SetViewport(g_DirectView);
-	if (GetAsyncKeyState(VK_RBUTTON) & 0x1)
 	{
 		WorldShader.projectView = g_newProjection;
 		g_ScreenChanged = false;
-		if (dest.Contains(CUR.x, CUR.y))
-		{
-			sound->Play();
-			textureSwitch = !textureSwitch;
-			lightsToggle = true;
-		}
 	}
 
 	g_thirdDirectView.Width = _swapChainDesc.BufferDesc.Width * 0.5f;
@@ -2810,6 +2790,7 @@ void DEMO_APP::thirdViewPort()
 	g_pd3dDeviceContext->RSSetState(SkyBoxRasterState);
 	g_pd3dDeviceContext->DrawIndexed(PlaneIndexCount, 0, 0);
 #pragma endregion End of Drawing the Floor
+
 
 }
 
